2d3374e2dc79ec6477dbcdf795921e907e3a88db
Eliana995 <eliana.ag.95@gmail.com>
2021-05-16 18:39:19 -0300

    algunos cambios

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const moduloTareas = require('./tareas/funcionesDeTareas');
 const process = require('process');
+const { listarTareas, guardarTarea, leerPorEstado } = require('./tareas/funcionesDeTareas');
 const texto = process.argv[2];
 
 switch (texto) {
-    case 'agregar':
-        let titulo = process.argv[3];
-        let estado = process.argv[4];
-        moduloTareas.agregarTarea(titulo,estado) 
+
+    case 'crear':   //original: agregar
+            let titulo = process.argv[3];
+            let estado = 'pendiente';
+            moduloTareas.agregarTarea(titulo,estado) 
         break;
     
     case 'listar':
-        moduloTareas.listarTareas()
+        listarTareas()
+        break;
+
+    case 'filtrar':
+        leerPorEstado(process.argv[3]);
         break;
 
     case undefined:
--- tareas/funcionesDeTareas.js
+++ tareas/funcionesDeTareas.js
@@ -3,23 +3,47 @@ module.exports = {
     leerJson : function() {   
         return JSON.parse(fs.readFileSync('./db/tareas.json', 'utf-8')) 
     },
-    agregarTarea : function(titulo, estado) {  
+    crearTarea : function(titulo, estado = 'pendiente') {   //original: agregarTarea
         let nuevaTarea = {
             titulo, 
-            estado
+            estado 
         }
         let tareas = this.leerJson();
-        tareas.push(nuevaTarea)
+        tareas.push(nuevaTarea);
+        this.guardarJson(tareas);
+        this.listarTareas();
     },
     guardarJson : function (tareas){
         fs.writeFileSync('./db/tareas.json',JSON.stringify(tareas),'utf-8')
         this.guardarJson(tareas)
     },
-    listarTareas : function(){ 
-        let tareas = this.leerJson();
-        for (let i = 0; i < tareas.length; i++) {
+    filtrarTareas : function(filtrar){
+        let filtradas = tareas.filter (tarea => tarea.estado === filtrar)
+        //return console.log(filtradas);
+    },
+    listarTareas : function(tareas){ 
+       // let tareas = this.leerJson();
+        tareas.forEach(tarea => {
+            console.log(tarea);
+        });
+       // for (let i = 0; i < tareas.length; i++) {
             
-            console.log(tareas[i]);  
-        }
+           // console.log(tareas[i]);  
+     //   }
+    },
+    escribirJson : function(tareas){
+        fs.writeFileSync('./db/tareas.json',JSON.stringify(tareas),'utf-8') //stringfico tareas
+    
+    },
+    guardarTarea : function(tarea){
+        let tareas = this.leerJson()
+        tareas.push(tarea)  //le pusheo la nueva tarea al array de objetos
+        this.escribirJson(tareas)
+    },
+    leerPorEstado : function(estado){
+        let tareas = this.leerJson();
+        this.filtrarTareas()
+        let filtradas = tareas.filter (tarea => tarea.estado === estado)
+        return tareas.listarTareas(filtradas);
     }
 } 
